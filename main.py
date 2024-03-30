46d22df60791bf7116bd8beb9d5c02f60d0a94a4
José Alves <josealves3416@gmail.com>
2024-03-30 15:42:36 -0300

    verificação de declaração

--- main.py
+++ main.py
@@ -2,21 +2,39 @@ class Stack:
   #construtor
   def __init__(self):
     self.stack = list()
+  def top(self):
+    if len(self.stack) > 0:
+      return self.stack[len(self.stack)-1]
+  def subtop(self):
+    if len(self.stack) > 1:
+      return self.stack[len(self.stack) - 2]
   #push stack
-  def push_stack(self, value):
-    self.stack.append(value)
+  def push_stack(self, value = '$', type = '$', scope = '$'):
+      self.stack.append({'value':value, 'type': type, 'scope': scope})
   #pop 
   def pop_stack(self):
-    self.stack.pop()
+    if len(self.stack) > 0:
+      self.stack.pop()
   #search by value
   def search(self, value):
     if len(self.stack) > 0:
-      for v in reversed(self.stack):
-        if v == value:
+      for l in reversed(self.stack):
+        if value == l['value']:
+          return l['type'], True
+      return "null", False
+    else:
+      print("Empty stack")
+
+  #search, but only for declaration
+  def search_var(self, value, scope):
+    if len(self.stack) > 0:
+      for l in reversed(self.stack):
+        if value == l['value'] and scope == l['scope']:
           return True
       return False
     else:
       print("Empty stack")
+
 
 class Token:
   def __init__(self, token, tipo, linha) -> None:
@@ -30,7 +48,9 @@ class Sintatico:
     self.posicao = 0
     self.erros = list()
     self.token = tokens[0]
-
+    self.pilha = Stack()
+    self.scope = 0
+    self.x = 0
   #Avança token
   def next(self):
     if(self.posicao + 1 < len(self.tokens)):
@@ -40,8 +60,11 @@ class Sintatico:
   #program inicia e terminará a análise
   def program(self):
     if self.token.token == "program":
+      #Primeiro elemento de pilha
+      self.pilha.push_stack("$")
       self.next()  
       if self.token.tipo == "id":
+        self.pilha.push_stack(self.token.token, "program", self.scope)
         self.next()
         if self.token.token == ";":
           self.next()
@@ -73,7 +96,6 @@ class Sintatico:
     if self.token.token == ":":
       self.next()
       self.tipo()
-      
       if self.token.token != ";":
         self.erros.append(f"; missing, line {self.tokens[self.posicao - 1].linha}")
       else:
@@ -86,6 +108,15 @@ class Sintatico:
     
   def lista_indentificadores(self):
     if self.token.tipo in ["id", "keyword"]:
+      #escopo inicial
+      self.pilha.push_stack("var_marker")
+      #verifica se já foi declarado, se for, então há erro de redeclaração  
+      if not self.pilha.search_var(self.token.token, self.scope):
+        self.pilha.push_stack(self.token.token, "unknown", self.scope)
+      #não foi declarado
+      else:
+        self.erros.append(f"{self.token.token} has already been declared, line {self.token.linha}")
+
       if self.token.tipo == "keyword":
         self.erros.append(f"{self.token.token} is a keyword, line {self.token.linha}")
       self.next()
@@ -97,6 +128,14 @@ class Sintatico:
   def lista_indentificadores2(self): #,id(ldi')/ ε
     if self.token.token == ",":
       self.next()
+      
+      #para semântico, se esperar outro(s) id(s)
+      if not self.pilha.search_var(self.token.token, self.scope):
+        self.pilha.push_stack(self.token.token, "unknown", self.scope)
+      #não foi declarado
+      else:
+        self.erros.append(f"{self.token.token} has already been declared, line {self.token.linha}")
+      
       #se espera um id
       if self.token.tipo in ["id", "keyword"]:
         if self.token.tipo == "keyword":
@@ -111,6 +150,8 @@ class Sintatico:
   
   def tipo(self):
     if(self.token.token in ['real','integer','boolean']):
+      self.atribuirTipo()
+
       self.next()
     elif self.token.tipo != "nls":
       self.erros.append(f"{self.token.token}: Tipo inválido")
@@ -129,6 +170,14 @@ class Sintatico:
 
   def declaracao_de_subprograma(self): #d_subprgm
     if self.token.tipo == "id":
+      #se não foi declarado nada igual no mesmo escopo
+      if not self.pilha.search_var(self.token.token, self.scope):
+        self.pilha.push_stack(self.token.token, "procedure", self.scope)
+      #já foi declarado algo com esse id no mesmo escopo
+      else:
+        self.erros.append(f"{self.token.token} have already been declared")
+      self.pilha.push_stack("$")
+      self.scope += 1
       self.next()
     else:
       self.erros.append(f"ID inválido, {self.token.token}")
@@ -175,7 +224,6 @@ class Sintatico:
     
     self.tipo()
     self.lista_de_parametros2()
-
   
   def lista_de_parametros2(self):
     if self.token.token in ";,":
@@ -200,12 +248,18 @@ class Sintatico:
 
   def comando_composto(self):
     if self.token.token == "begin":
+      self.x += 1
       self.next()
       self.comandos_opcionais()
       if self.token.token == "end":
         self.next()
       elif self.token.tipo != "nls":
         self.erros.append(f"Expected end, before {self.token.token}, line {self.token.linha}")
+      #garantindo o fim do escopo, iremos limpá-lo da pilha
+      self.x -= 1
+      if self.x == 0:
+        self.cleanScope()
+    
     elif self.token.tipo != "nls":
       self.erros.append(f"begin expected {self.token.linha}, but started with {self.token.token}")
 
@@ -225,6 +279,13 @@ class Sintatico:
   def comando(self):
     #se lê ID
     if self.token.tipo == "id":
+      tipo, declared = self.pilha.search(self.token.token)
+      #Não achou na pilha de declaração
+      if not declared:
+        self.erros.append(f"{self.token.token} was not declared, line {self.token.linha}")
+      #Achou, mas é identificador de programa
+      elif tipo == "program":
+        self.erros.append(f"{self.token.token} is program identifier")
       self.next()
       self.ativacao_de_procedimento()
     
@@ -290,6 +351,14 @@ class Sintatico:
       self.next()
       #mesmo se estiver errado (keyword) ele entra, pois pode ter escrito errado
       if self.token.tipo in ["id","keyword"]:
+        tipo, declared = self.pilha.search(self.token.token)
+        #Não achou na pilha de declaração
+        if not declared:
+          self.erros.append(f"{self.token.token} was not declared, line {self.token.linha}")
+        #Achou, mas é identificador de programa
+        elif tipo == "program":
+          self.erros.append(f"{self.token.token} is program identifier")
+
         if self.token.tipo == "keyword":
           self.erros.append(f"{self.token.token} is a keyword")
         self.next()
@@ -416,6 +485,14 @@ class Sintatico:
 
     #id
     elif self.token.tipo == "id":
+      tipo, declared = self.pilha.search(self.token.token)
+      #Não achou na pilha de declaração
+      if not declared:
+        self.erros.append(f"{self.token.token} was not declared, line {self.token.linha}")
+      #Achou, mas é identificador de programa
+      elif tipo == "program":
+        self.erros.append(f"{self.token.token} is program identifier")
+
       self.next()
       #id(expressão)
       if self.token.token == "(":
@@ -448,6 +525,24 @@ class Sintatico:
     else:
       pass
 
+  def atribuirTipo(self):
+    for index in range(len(self.pilha.stack)-1, 0, -1):
+        if self.pilha.stack[index]['value'] == "var_marker":
+          self.pilha.stack.pop(index)
+          break
+        self.pilha.stack[index]['type'] = self.token.token
+
+  def cleanScope(self):
+    while self.pilha.top()['value'] != "$":
+      self.pilha.pop_stack()
+      #Limpa marcador
+    
+    self.scope -= 1
+    #Limpa marcador
+    self.pilha.pop_stack()
+      
+    
+    
     
 #Leitura dos tokens -> token | tipo
 tokensFile = open("tabela2.csv", "r")
@@ -462,7 +557,6 @@ for line in lines:
   line = line.split(" ")
   tokens.append(Token(line[0], line[1], line[2].replace("\n","")))
 
-
 app = Sintatico(tokens)
 app.program()
 
@@ -471,4 +565,8 @@ if len(app.erros):
   for erro in app.erros:
     print(f'\033[31m"{erro}"\033[m')
 else:
-  print("A análise sintática não encontrou erros!")
+  print("A análise sintática/semântica não encontrou erros!")
+
+
+
+
